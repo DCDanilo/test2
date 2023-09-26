357691d6fed165d66e05cb41c511440680c5099f
DCDanilo <danilo.decave@gmail.com>
2023-09-26 18:15:16 +0200

    pt2

--- assets/js/custom/main.js
+++
@@ -1,8 +0,0 @@
-let collapseButton = document.querySelector('#button_collapse');
-collapseButton.addEventListener('click', function(){
-    let collapseMenu = document.querySelector('#gestione_utenti');
-  
-    if(!collapseMenu.hasAttribute('class', 'show')){
-        console.log(collapseMenu.classList);
-    }
-})
